2bc33122558cfc4beeff0a0880f13f8f3c5babf4
Benji <thebenjimoto@gmail.com>
2019-10-02 22:32:50 -0400

    Finish Login/signup page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import NavbarContainer from './navbar/navbar_container'
 import { Route, Switch, Link } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
 
 class App extends React.Component {
   render(){
@@ -12,6 +13,7 @@ class App extends React.Component {
         <NavbarContainer />
       </header>
         <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </div>
     )
   }
--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class LoginForm extends React.Component {
     constructor(props) {
@@ -10,7 +11,14 @@ class LoginForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    
     //TODO componentdidupdate or componentdidmount to handle re-rendering errors
+    // componentDidUpdate(prevProps, prevState) {
+
+    // }
+    componentDidMount() {
+        this.props.clearErrors();
+    }
 
     handleChange(field) {
         return e => this.setState({ [field]: e.target.value });
@@ -22,25 +30,42 @@ class LoginForm extends React.Component {
     }
 
     renderErrors() {
-        return (
-            <ul>
-                {this.props.errors.map((error, idx) => (
-                    <li key={`error-${idx}`}>{error}</li>
-                ))}
-            </ul>
-        )
+        if (this.props.errors.length > 0) {
+            return (
+                <ul className="errors-list-flash">
+                    {this.props.errors.map((error, idx) => (
+                        <li key={`error-${idx}`}>{error}</li>
+                    ))}
+                </ul>
+            )
+        } else {
+            return (
+                <ul className="errors-list">
+                    {this.props.errors.map((error, idx) => (
+                        <li key={`error-${idx}`}>{error}</li>
+                    ))}
+                </ul>
+            )
+        }
     }
 
     render() {
         return (
+            <>
+            { this.renderErrors() }
+                <div className="signup-login-header">
+                    <p className="frap-join">Hello friend!</p>
+                    <p className="frap-join-intro">It's good to have you back. Sign in here and find your next frap time!</p>
+                </div> 
             <div className="signup-login-form-container">
-            {this.renderErrors()}
             <form onSubmit={this.handleSubmit}>
                 <input type="text" value={this.state.username} placeholder="Username" onChange={this.handleChange('username')}/>
                 <input type="password" value={this.state.password} placeholder="Password" onChange={this.handleChange('password')}/>
-                <input type="submit" value="sign in" />
+                        <input type="submit" className="signup-login-submit" value="SIGN IN" />
+                <Link to='/signup' className='login-signup-redirect'>If you've never signed up before, click here</Link>
             </form>
             </div>
+            </>
         )
     }
 }
