8f988905b527ec75eea4ec7a7637925c135ff20a
Benji <thebenjimoto@gmail.com>
2019-10-06 00:55:10 -0400

    work on map

--- frontend/components/event/EventMap.jsx
+++ frontend/components/event/EventMap.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
-import MarkerManager from '../../util/marker_manager';
 
 class EventMap extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            center: { lat: 37.77589, lng: -122.345 },
+            zoom: 10
+        }
     }
 
     componentDidMount() {
-        const mapOptions = {
-            center: { lat: 37.7758, lng: -122.435 },
-            zoom: 13
-        };
+        const mapOptions = this.state;
 
-        // wrap this.mapNode in a Google Map
+
         this.map = new google.maps.Map(this.mapNode, mapOptions);
-        this.MarkerManager = new MarkerManager(this.map);
-        this.MarkerManager.updateMarkers();
+        // this.MarkerManager = new MarkerManager(this.map);
+        // this.MarkerManager.updateMarkers();
     }
 
-    componentDidUpdate() {
-        this.MarkerManager.updateMarkers();
-    }
+    // componentDidUpdate() {
+    //     this.MarkerManager.updateMarkers();
+    // }
 
     render() {
         return (
             <div id='map-container' ref={map => this.mapNode = map}>
             </div>
-             // this ref gives us access to the map dom node
         )
     }
 }
