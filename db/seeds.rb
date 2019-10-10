0ad36da4532cfd82280c3766ff42dc1b052f8522
Benji <TheBenjimoto@gmail.com>
2019-10-10 15:06:41 -0400

    fixed seeding'

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,36 +17,45 @@ u4 = User.create!(username: "Mononoke", email: "Mononoke@PrincessMononoke.com", 
 u5 = User.create!(username: "Jojo", email: "Jojo@jojo.com", password: "jojosbizzareadventure")
 u6 = User.create!(username: "Dio", email: "Dio@dio.com", password: "jojosbizzareadventure")
 
-e1 = Event.create!(name: "Witch gathering", openings: 3, lat: 40.747149, lng: -73.989218, host_id: u1.id, date: "Saturday Oct 12", time: "12PM")
-# e2 = Event.create!(name: "Fuzzy get together", openings: 5, lat: 40.748359, lng: -73.985008, host_id: u1.id, date: "Sunday, Oct 13", time: "1AM")
-# e3 = Event.create!(name: "Castle time", openings: 4, lat: 40.741139, lng: -74.001219, host_id: u3.id, date: "Monday, Oct 14", time: "4PM") 
-e4 = Event.create!(name: "Come get your free breakfast", openings: 2, lat: 40.741139, lng: -74.001219, host_id: u3.id, date: "Tuesday Oct 15", time: "7AM")
-e5 = Event.create!(name: "Dio Time", openings: 1, lat: 40.751416, lng: -73.984223, host_id: u5.id, date: "Tuesday Oct 15", time: "7AM")
-e6 = Event.create!(name: "Jojo Time", openings: 5, lat: 37.798954, lng: -122.401502, host_id: u6.id, date: "Tuesday Oct 15", time: "7AM", 
+e1 = Event.new(name: "Witch gathering", openings: 3, lat: 40.747149, lng: -73.989218, host_id: u1.id, date: "Saturday Oct 12", time: "12PM")
+# e2 = Event.new(name: "Fuzzy get together", openings: 5, lat: 40.748359, lng: -73.985008, host_id: u1.id, date: "Sunday, Oct 13", time: "1AM")
+# e3 = Event.new(name: "Castle time", openings: 4, lat: 40.741139, lng: -74.001219, host_id: u3.id, date: "Monday, Oct 14", time: "4PM") 
+e4 = Event.new(name: "Come get your free breakfast", openings: 2, lat: 40.741139, lng: -74.001219, host_id: u3.id, date: "Tuesday Oct 15", time: "7AM")
+e5 = Event.new(name: "Dio Time", openings: 1, lat: 40.751416, lng: -73.984223, host_id: u5.id, date: "Tuesday Oct 15", time: "7AM")
+e6 = Event.new(name: "Jojo Time", openings: 5, lat: 37.798954, lng: -122.401502, host_id: u6.id, date: "Tuesday Oct 15", time: "7AM", 
 summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum commodo condimentum. Aenean non est ut neque iaculis rhoncus vel sed purus.", 
 story: "Duis scelerisque neque aliquet dui ornare tincidunt. Cras aliquam tellus eget ante scelerisque tincidunt.", 
 discussion: "Phasellus lorem justo, euismod a arcu sed, tincidunt ultrices sapien.",  
 quote: "Quisque ultrices molestie metus non finibus.")
-e7 = Event.create!(name: "Going for broke", openings: 2, lat: 32.790808, lng: -96.797194, host_id: u4.id, date: "Tuesday Oct 15", time: "7AM")
-e8 = Event.create!(name: "Princesses only", openings: 3, lat: 42.377372, lng: -71.116808, host_id: u2.id, date: "Tuesday Oct 15", time: "7AM")
+e7 = Event.new(name: "Going for broke", openings: 2, lat: 32.790808, lng: -96.797194, host_id: u4.id, date: "Tuesday Oct 15", time: "7AM")
+e8 = Event.new(name: "Princesses only", openings: 3, lat: 42.377372, lng: -71.116808, host_id: u2.id, date: "Tuesday Oct 15", time: "7AM")
+
+file1 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/kiki.png')
+file2 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/totoro.jpg')
+file3 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/truehowl.jpeg')
+file4 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/mononoke.jpg')
+file5 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/jojo.png')
+file6 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/dio.png')
+
+e1.photo.attach(io: file1, filename: 'kiki.png')
+e4.photo.attach(io: file3, filename: 'truehowl.jpeg')
+e5.photo.attach(io: file5, filename: 'jojo.png')
+e6.photo.attach(io: file6, filename: 'dio.png')
+e7.photo.attach(io: file4, filename: 'mononoke.jpg')
+e8.photo.attach(io: file2, filename: 'totoro.jpg')
+
+# e2.photo.attach(io: file1, filename: 'kiki.png')
+# e3.photo.attach(io: file3, filename: 'howl.png')
+
+e1.save!
+e4.save!
+e5.save!
+e6.save!
+e7.save!
+e8.save!
 
 a1 = Attendee.create!(user_id: u3.id, event_id: e4.id)
 a2 = Attendee.create!(user_id: u2.id, event_id: e1.id)
 a3 = Attendee.create!(user_id: u1.id, event_id: e4.id)
 a4 = Attendee.create!(user_id: u4.id, event_id: e5.id)
 
-file1 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/kiki.png')
-file2 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/totoro.jpg')
-file3 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/howl.jpg')
-file4 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/mononoke.jpg')
-file5 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/jojo.png')
-file6 = open('https://frapwithfriends-seeds.s3.us-east-2.amazonaws.com/dio.png')
-
-e1.photo.attach(io: file1, filename: 'kiki.png')
-e8.photo.attach(io: file2, filename: 'totoro.jpg')
-e4.photo.attach(io: file3, filename: 'howl.jpg')
-e7.photo.attach(io: file4, filename: 'mononoke.jpg')
-e5.photo.attach(io: file5, filename: 'jojo.png')
-e6.photo.attach(io: file6, filename: 'dio.png')
-# e2.photo.attach(io: file1, filename: 'kiki.png')
-# e3.photo.attach(io: file3, filename: 'howl.png')
