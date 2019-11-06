fd5fe84ae9047753b04213de6c6110a18b81211e
benjibooty <thebenjimoto@gmail.com>
2019-11-06 18:05:15 -0500

    Work on delete attendee

--- frontend/reducers/attendees_reducer.js
+++ frontend/reducers/attendees_reducer.js
@@ -1,10 +1,13 @@
-import { RECEIVE_ATTENDEE } from '../actions/attendee_actions';
+import { RECEIVE_ATTENDEE, DELETE_ATTENDEE } from '../actions/attendee_actions';
 
 const attendeesReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ATTENDEE:
       return action.attendee;
+    case DELETE_ATTENDEE:
+      return action.attendee;
+      // todo
     default:
       return state;
   }
