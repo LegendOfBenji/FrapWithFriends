3a3c7ead6df967f4381714d1ad315097b425d9f0
benjibooty <thebenjimoto@gmail.com>
2019-11-05 21:33:44 -0500

    Right before delete actions and styling

--- app/views/api/users/index.json.jbuilder
+++ app/views/api/users/index.json.jbuilder
@@ -3,11 +3,19 @@ json.partial! "api/users/user", user: @user
 json.events do
     json.array!(@events) do |attended_event|
         json.photoUrl url_for(attended_event.photo)
+        json.username attended_event.host.username
+        json.date attended_event.date
+        json.time attended_event.time
+        json.name attended_event.name
     end
 end
 
 json.hosted_events do
     json.array!(@hosted_events) do |hosted_event|
         json.photoUrl url_for(hosted_event.photo)
+        json.username hosted_event.host.username
+        json.date hosted_event.date
+        json.time hosted_event.time
+        json.name hosted_event.name
     end
 end
--- frontend/components/profile/ProfileShow.jsx
+++ frontend/components/profile/ProfileShow.jsx
@@ -7,36 +7,56 @@ class ProfileShow extends React.Component {
 
   componentDidMount(){
     this.props.fetchUser();
-    console.log(this.props.user)
   }
 
   joined() {
     if (this.props.user.events) {
-    return this.props.user.events.map(event => {
+    if (this.props.user.events.length > 0) {
+      return this.props.user.events.map(event => {
       return (
         <div>
           <img src={event.photoUrl}/>
         </div>
       )
-    })
+    }) 
+   } else { 
+     return (
+      <div>
+        <p>You don't have any events that you have joined. <a href='#/fraptimes'>Click here to see all existing events.</a></p>
+      </div>
+    )
+     }
   }
   }
 
   hosted() {
     if (this.props.user.hosted_events) {
-    return this.props.user.hosted_events.map(event => {
+      if (this.props.user.hosted_events.length > 0) {
+        return this.props.user.hosted_events.map(event => {
       return (
         <img src={event.photoUrl}/>
       )
     })
+   } else {
+     return (
+    <p>You don't have any hosted events. <a href='#/hosting'>You can host an event here!</a></p>
+     )
+    }
   }
   }
 
   render() {
     return (
       <>
+      <div className="event-index-header">
+                <p className="show-header-one">Welcome Home, {this.props.user.username}!</p>
+                <p className="show-header-two">What are you grateful for today?</p>
+            </div>
+            <div className="zero-profile"></div>
+      <div className="profile-div">
       <div>{this.joined()}</div>
       <div>{this.hosted()}</div>
+      </div>
       </>
     )
   }
